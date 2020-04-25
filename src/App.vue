4099ab96bb98a7c29237b1631610f7136603e758
xuruiting2 <xu947793331@163.com>
2020-04-25 19:23:17 +0800

    app.vue进行更新

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
       <input type="text" class="search" v-model="list" @keyup.enter="add">
       <h3>待办事项</h3>
       <ul class="firstlist">
-        <li v-for='(item, index) in doList' :key="item" @click='dele(item, index)'>
-          <input type="checkbox" v-model="item.checked">
-          {{item.text}}
+        <li v-for='(item, index) in doList' :key="item" @click="message(index)">
+          <input type="checkbox" v-model="item.checked" @click='dele(item, index)'>
+          <span class="text">{{item.text}}</span>
           <span class="time">{{item.date}}</span>
           </li>
       </ul>
@@ -15,14 +15,17 @@
       <ul class="secondlist">
         <li v-for='item in finList' :key="item" >
           <input type="checkbox" disabled>
-          <span :class="{active:item.checked}">{{item.text}}</span>
+          <span :class="{active:item.checked,text:true}">{{item.text}}</span>
           <span class="time">{{item.mydate | times}}</span>
           </li>
       </ul>
     </div>
+    <div class="message" v-if='isShow'>
+      <h3>详情</h3>
+      <p>{{pasege}}</p>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'App',
@@ -34,7 +37,9 @@ export default {
       finList: [],
       currentTime: '',
       // mydate: '',
-      timeList: []
+      timeList: [],
+      pasege: '',
+      isShow: false
     }
   },
   methods: {
@@ -67,6 +72,11 @@ export default {
       this.finList.push(item)
       this.doList.splice(index, 1)
       item.mydate = new Date()
+    },
+    message (index) {
+      // eslint-disable-next-line no-unused-vars
+      this.isShow = true
+      this.pasege = this.doList[index].text
     }
   },
   filters: {
@@ -78,10 +88,21 @@ export default {
 </script>
 
 <style>
+#app{
+  display: flex;
+  width: 70%;
+}
+.message {
+  width: 49%;
+  height: 500px;
+  /* border:solid 1px rebeccapurple; */
+  margin: 50px 0px 0px 0px;
+}
 .wrap {
-  width: 500px;
+  width: 50%;
   height: 500px;
   margin: 50px 0px 0px 50px;
+  /* border: solid 1px rebeccapurple; */
 }
 * {
   margin: 0;
@@ -103,6 +124,8 @@ ul {
 li {
   font-size: 18px;
   margin: 10px 0px;
+  /* border: solid 1px saddlebrown; */
+  width: 100%;
 }
 .active {
   color: gray;
@@ -112,5 +135,17 @@ li {
   color: gray;
   font-size: 16px;
   float: right;
+  /* border: solid 1px rosybrown; */
+}
+.text{
+  display: inline-block;
+  /* border: solid 1px saddlebrown; */
+  width: 40%;
+  text-overflow:ellipsis;
+  white-space:nowrap;
+  overflow:hidden;
+}
+p {
+  margin-top: 20px;
 }
 </style>
